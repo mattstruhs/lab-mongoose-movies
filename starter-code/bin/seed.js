20b3b9fb0e0f3e7f20d52195a907d243d62f789c
Matt Struhs <mattstruhs@yahoo.com>
2021-03-24 06:51:57 -0400

    movies complee without test

--- starter-code/bin/seed.js
+++ starter-code/bin/seed.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const Celebrity = require("../models/celebrity");
+const Movie = require('../models/movie')
 
-const array = [
+const celebArray = [
   {
     name: "Robert Downey Jr",
     occupation: "actor",
@@ -19,6 +20,24 @@ const array = [
   },
 ];
 
+const movieArray = [
+  {
+    title: "title1",
+    genre: "genre1",
+    plot: "plot1",
+  },
+  {
+    title: "title2",
+    genre: "genre2",
+    plot: "plot2",
+  },
+  {
+    title: "title4",
+    genre: "genre3",
+    plot: "plot3",
+  },
+];
+
 // establish a connection to the db
 mongoose
   .connect("mongodb://localhost/starter-code", { useNewUrlParser: true })
@@ -26,12 +45,18 @@ mongoose
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
     );
-    Celebrity.create(array)
+    Celebrity.create(celebArray)
       .then((celebritiesFromDB) => {
         console.log(`${celebritiesFromDB.length} celebrites have been added`);
         mongoose.connection.close();
       })
       .catch((err) => console.log("err", err));
+    Movie.create(movieArray)
+    .then((celebritiesFromDB) => {
+      console.log(`${celebritiesFromDB.length} celebrites have been added`);
+      mongoose.connection.close();
+    })
+    .catch((err) => console.log("err", err));
   })
   .catch((err) => {
     console.error("Error connecting to mongo", err);
